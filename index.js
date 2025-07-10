e053f4e4bf353954961a6083ca7ae9da3df17a77
Raahim Wajid Ali <Raahimwajid21@gmail.com>
2025-07-10 20:46:02 +0500

    Connected MongoDB and updated deployment settings

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: "https://super-cheesecake-bd5837.netlify.app", // your React frontend
+    origin: "https://splendid-naiad-4c1685.netlify.appl", // your React frontend
     credentials: true,
   })
 );
